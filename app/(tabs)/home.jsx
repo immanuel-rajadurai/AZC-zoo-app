719b418fe996e8e6d419ea96145bcb585143e378
MedantSharan <medsharanofficial@gmail.com>
2024-08-04 23:12:51 +0530

    Added a working modular that opens on tapping animal markers and shows information about the animal

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -1,34 +1,41 @@
-import { View, Text, FlatList, Image, RefreshControl, Alert, StyleSheet, Button, TouchableOpacity  } from 'react-native'
-import React, { useEffect, useState, useRef  } from 'react'
-import { SafeAreaView } from 'react-native-safe-area-context'
-import { images } from '../../constants'
-import SearchInput from '../../components/searchInput'
-import Events from '../../components/Events'
-import EmptyState from '../../components/EmptyState'
-import { getAllPosts, getLatestPosts } from '../../lib/appwrite'
-import useAppwrite from '../../lib/useAppwrite'
-import MapView, { Overlay } from 'react-native-maps';
-import { Marker } from "react-native-maps";
+import { View, Text, Image, StyleSheet, TouchableOpacity, Modal, ScrollView } from 'react-native';
+import React, { useState, useRef, useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import MapView, { Marker, Overlay } from 'react-native-maps';
+import * as Location from 'expo-location';
+import CustomButton from '../../components/CustomButton';
 import { mapstyle1 } from "../../styling/mapstyles";
-import CustomButton from '../../components/CustomButton'
-import * as Location from "expo-location";
 
 const Home = () => {
-
-  // const { data: posts, refetch } = useAppwrite(getAllPosts);
-
-  var posts = [{"$collectionId": "668e5083002b3f534cf3", "$createdAt": "2024-07-12T10:17:03.558+00:00", "$databaseId": "668e503d0036733ee658", "$id": "669102a0003c95af39aa", "$permissions": [], "$tenant": "165998", "$updatedAt": "2024-07-12T10:17:03.558+00:00", "prompt": "Picture the future of coding with AI. Show AR VR", "thumbnail": "https://wildlife.foothillsclusters.com/wp-content/uploads/2023/05/230518-03.jpg", "title": "How AI Shapes Coding Future", "users": {"$collectionId": "668e5057003aed7c3b16", "$createdAt": "2024-07-10T14:46:56.037+00:00", "$databaseId": "668e503d0036733ee658", "$id": "668e9ee100305aae9daf", "$permissions": [Array], "$tenant": "165998", "$updatedAt": "2024-07-10T14:46:56.037+00:00", "accountId": "668e9edf001c2f7b6117", "avatar": "https://cloud.appwrite.io/v1/avatars/initials?name=immraj&project=668e4f04003bfae2a718", "email": "immanuelraj154@gmail.com", "username": "immraj"}, "video": "https://player.vimeo.com/video/949581999?h=4672125b31"}, {"$collectionId": "668e5083002b3f534cf3", "$createdAt": "2024-07-12T10:17:47.089+00:00", "$databaseId": "668e503d0036733ee658", "$id": "669102cc0021f55940c3", "$permissions": [], "$tenant": "165998", "$updatedAt": "2024-07-12T10:17:47.089+00:00", "prompt": "Create a motivating AI driven video aimed at inspiring coding enthusiasts with simple language", "thumbnail": "https://cms.whipsnadezoo.org/sites/default/files/styles/responsive/public/1024/1024/0/2022-12/Meet-the-Animals-24-Photo-by-Justin-Doherty.jpg.webp", "title": "Get inspired to code", "users": {"$collectionId": "668e5057003aed7c3b16", "$createdAt": "2024-07-10T14:46:56.037+00:00", "$databaseId": "668e503d0036733ee658", "$id": "668e9ee100305aae9daf", "$permissions": [Array], "$tenant": "165998", "$updatedAt": "2024-07-10T14:46:56.037+00:00", "accountId": "668e9edf001c2f7b6117", "avatar": "https://cloud.appwrite.io/v1/avatars/initials?name=immraj&project=668e4f04003bfae2a718", "email": "immanuelraj154@gmail.com", "username": "immraj"}, "video": "https://player.vimeo.com/video/949579770?h=897cd5e781"}, {"$collectionId": "668e5083002b3f534cf3", "$createdAt": "2024-07-12T10:18:22.338+00:00", "$databaseId": "668e503d0036733ee658", "$id": "669102ef003c49d4c7bd", "$permissions": [], "$tenant": "165998", "$updatedAt": "2024-07-12T10:18:22.338+00:00", "prompt": "Create a heartwarming video following the travels of dalmatian dog exploring beautiful Italy", "thumbnail": "https://cdn.britannica.com/83/195983-138-66807699/numbers-tiger-populations.jpg?w=800&h=450&c=crop", "title": "Dalmatian's journey through Italy", "users": {"$collectionId": "668e5057003aed7c3b16", "$createdAt": "2024-07-10T14:46:56.037+00:00", "$databaseId": "668e503d0036733ee658", "$id": "668e9ee100305aae9daf", "$permissions": [Array], "$tenant": "165998", "$updatedAt": "2024-07-10T14:46:56.037+00:00", "accountId": "668e9edf001c2f7b6117", "avatar": "https://cloud.appwrite.io/v1/avatars/initials?name=immraj&project=668e4f04003bfae2a718", "email": "immanuelraj154@gmail.com", "username": "immraj"}, "video": "https://player.vimeo.com/video/949582778?h=d60220d68d"}]
-
-  const [refreshing, setRefreshing] = useState(false)
-  const mapRef = useRef(null); 
+  const mapRef = useRef(null);
   const [currentLocation, setCurrentLocation] = useState(null);
+  const [modalVisible, setModalVisible] = useState(false);
+  const [selectedAnimal, setSelectedAnimal] = useState(null);
 
-
-  const onRefresh = async () => {
-    setRefreshing(true);
-    // await refetch();
-    setRefreshing(false);
-  }
+  const animals = [
+    {
+      name: 'Hippopotamus',
+      species: 'Hippopotamus amphibius',
+      coordinates: { latitude: 51.535121, longitude: -0.154131 },
+      image: require("../../assets/amimalicons/hippo.png"),
+      facts: 'Hippos are the third largest land mammal after elephants and rhinos.\n\nHippos are found in Savannahs!\n\nOther Facts'
+    },
+    {
+      name: 'Tiger',
+      species: 'Panthera tigris',
+      coordinates: { latitude: 51.535121, longitude: -0.1547 },
+      image: require("../../assets/amimalicons/tiger.png"),
+      facts: 'Tigers are the largest wild cats in the world.\n\nTigers are apex predators!\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts'
+    },
+    {
+      name: 'Giraffe',
+      species: 'Giraffa camelopardalis',
+      coordinates: { latitude: 51.5355, longitude: -0.155 },
+      image: require("../../assets/amimalicons/giraffe.png"),
+      facts: 'Giraffes are the tallest mammals on Earth.\n\nGiraffes are herbivores!\n\nOther facts'
+    },
+  ];
+  
 
   const [region, setRegion] = useState({
     latitude: 48.7460,
@@ -37,13 +44,6 @@ const Home = () => {
     longitudeDelta: 0.01,
   });
 
-  // const zooRegion = {
-  //   latitude: 48.7460,
-  //   longitude: 2.66315,
-  //   latitudeDelta: 0.003,
-  //   longitudeDelta: 0.003,
-  // };
-
   const zooRegion = {
     latitude: 51.535121,
     longitude: -0.154131,
@@ -51,18 +51,19 @@ const Home = () => {
     longitudeDelta: 0.003,
   };
 
+
   useEffect(() => {
     const getLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
       if (status !== "granted") {
-        console.log("Permission to  access location was denied");
+        console.log("Permission to access location was denied");
         return;
       }
 
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation(location.coords);
 
-      setInitialRegion({
+      setRegion({
         latitude: location.coords.latitude,
         longitude: location.coords.longitude,
         latitudeDelta: 0.005,
@@ -74,102 +75,97 @@ const Home = () => {
   }, []);
 
   const goToZoo = () => {
-    //Animate the user to new region. Complete this animation in 3 seconds
     mapRef.current.animateToRegion(zooRegion, 500);
   };
-
-  const mapView = React.createRef();
-
-  const animateMap = () => {
-    mapView.current.animateToRegion(zooRegion, 1000); 
-  }
 
   const imageBounds = [
     [51.533119, -0.159473], // Southwest coordinates
     [51.536625, -0.151500], // Northeast coordinates
   ];
 
+  const handleAnimalPress = (animal) => {
+    setSelectedAnimal(animal);
+    setModalVisible(true);
+  };
+
   return (
     <View style={styles.container}>
-    <MapView
-      style={styles.map}
-      ref={mapRef}
-      initialRegion={zooRegion}
-      showsUserLocation={true}
-      customMapStyle={mapstyle1}
-      onRegionChangeComplete={(region) => setRegion(region)}
-      provider={MapView.PROVIDER_GOOGLE}
-    >
+      <MapView
+        style={styles.map}
+        ref={mapRef}
+        initialRegion={zooRegion}
+        showsUserLocation={true}
+        customMapStyle={mapstyle1}
+        onRegionChangeComplete={(region) => setRegion(region)}
+        provider={MapView.PROVIDER_GOOGLE}
+      >
+        <Overlay  
+          image={require("../../assets/mapoverlays/londonzoo.png")}
+          bounds={imageBounds}
+          bearing={0.6}
+          style={styles.overlay}
+          opacity={0.0}
+        />
 
-      <Overlay  
-        image={require("../../assets/mapoverlays/londonzoo.png")}
-        bounds = {imageBounds}
-        bearing = {0.6}
-        style = {styles.overlay}
-        opacity = {0.0}
-      />
-
-        <Marker
-          coordinate={{
-            latitude: 51.535121,
-            longitude: -0.154131,
-          }}
-        > 
-           <View>
+        {animals.map((animal, index) => (
+          <Marker
+          key={index}
+          coordinate={animal.coordinates}
+          onPress={() => handleAnimalPress(animal)}
+        >
             <Image
-              source={require("../../assets/amimalicons/hippo.png")}
-              style={{width: 60, height: 60}}
+              source={animal.image}
+              style={{ width: 60, height: 60 }}
               resizeMode="contain"
             />
+          </Marker>
+        ))}
+      </MapView>
+
+      <CustomButton handlePress={() => goToZoo()} title="Go to Zoo" />
+      <Text style={styles.text}>Current latitude: {region.latitude}</Text>
+      <Text style={styles.text}>Current longitude: {region.longitude}</Text>
+
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={modalStyles.centeredView}>
+          <View style={modalStyles.modalView}>
+            {selectedAnimal && (
+              <>
+                <Text style={modalStyles.topicText}>{selectedAnimal.name}</Text>
+                <Text style={modalStyles.subTopicText}>( {selectedAnimal.species} )</Text>
+                <Image
+                  source={selectedAnimal.image} 
+                  style={modalStyles.image}
+                />
+                <ScrollView style={modalStyles.scrollContainer}>
+                  <Text style={modalStyles.modalText}>{selectedAnimal.facts}</Text>
+                </ScrollView>
+                <TouchableOpacity
+                  style={[modalStyles.button, modalStyles.buttonClose]}
+                  onPress={() => setModalVisible(!modalVisible)}
+                >
+                  <Text style={modalStyles.textStyle}>Close</Text>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
-        </Marker>
-
-        <Marker
-          coordinate={{
-            latitude: 51.535121,
-            longitude: -0.1547,
-          }}
-        > 
-           <View>
-            <Image
-              source={require("../../assets/amimalicons/tiger.png")}
-              style={{width: 60, height: 60}}
-              resizeMode="contain"
-            />
-          </View>
-        </Marker>
-
-
-        <Marker
-          coordinate={{
-            latitude: 51.5355,
-            longitude: -0.155,
-          }}
-        > 
-           <View>
-            <Image
-              source={require("../../assets/amimalicons/giraffe.png")}
-              style={{width: 60, height: 60}}
-              resizeMode="contain"
-            />
-          </View>
-        </Marker>
-
-    </MapView>
-
-    <CustomButton handlePress={() => goToZoo()} title="Go to Zoo" />
-    {/* <TouchableOpacity onPress={animateMap}><Text>Start</Text></TouchableOpacity> */}
-    {/*Display user's current region:*/}
-    <Text style={styles.text}>Current latitude : {region.latitude}</Text>
-    <Text style={styles.text}>Current longitude: {region.longitude}</Text>
-  </View>
-  )
+        </View>
+      </Modal>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     ...StyleSheet.absoluteFillObject,
-    flex: 1, //the container will fill the whole screen.
+    flex: 1,
     justifyContent: "flex-end",
     alignItems: "center",
   },
@@ -179,30 +175,74 @@ const styles = StyleSheet.create({
   overlay: {
     transform: [{ rotate: '90deg' }],
   },
-});
- 
-const overlayStyles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    justifyContent: 'flex-end',
-    alignItems: 'center',
-  },
-  map: {
-    ...StyleSheet.absoluteFillObject,
-  },
-  overlay: {
-    transform: [{ rotate: '90deg' }],
+  text: {
+    fontSize: 16,
+    margin: 10,
   },
 });
 
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//   },
-//   map: {
-//     width: '100%',
-//     height: '100%',
-//   },
-// });
+const modalStyles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 20,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonClose: {
+    backgroundColor: "green",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  topicText: {
+    color: "green",
+    fontWeight: "bold",
+    fontSize: 20,
+    margin: 10,
+  },
+  subTopicText: {
+    color: "darkgreen",
+    fontSize: 16,
+    fontWeight: "bold",
+    fontStyle: "italic",
+    marginBottom: 10,
+  },
+  modalText: {
+    marginBottom: 30,
+    fontSize: 14,
+    fontWeight: "bold",
+    textAlign: "left",
+  },
+  image: {
+    width: 100,
+    height: 100,
+    marginBottom: 15,
+  },
+  scrollContainer: {
+    maxHeight: 100, // Set a max height for the scrollable area
+  },
+});
 
-export default Home
+export default Home;
