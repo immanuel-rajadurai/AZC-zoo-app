ddbd2c80992b28c719fb9335153822c8bcdfd361
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-08-16 21:16:44 +0100

    move redundant files out of code

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -16,32 +16,6 @@ import { listEvents } from '../../src/graphql/queries';
 const client = generateClient();
 
 
-// const animals = [
-//   {
-//     key: "1",
-//     name: 'Hippopotamus',
-//     species: 'Hippopotamus amphibius',
-//     coordinates: { latitude: 51.535121, longitude: -0.154131 },
-//     image: require("../../assets/amimalicons/hippo.png"),
-//     facts: 'Hippos are the third largest  land mammal after elephants and rhinos.\n\nHippos are found in Savannahs!\n\nOther Facts'
-//   },
-//   {
-//     key: "2",
-//     name: 'Tiger',
-//     species: 'Panthera tigris',
-//     coordinates: { latitude: 51.535121, longitude: -0.1547 },
-//     image: require("../../assets/amimalicons/tiger.png"),
-//     facts: 'Tigers are the largest wild cats in the world.\n\nTigers are apex predators!\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts\n\nOther facts'
-//   },
-//   {
-//     key:"3",
-//     name: 'Giraffe',
-//     species: 'Giraffa camelopardalis',
-//     coordinates: { latitude: 51.5355, longitude: -0.155 },
-//     image: require("../../assets/amimalicons/giraffe.png"),
-//     facts: 'Giraffes are the tallest mammals on Earth.\n\nGiraffes are herbivores!\n\nOther facts'
-//   },
-// ];
 
 const Home = () => {
   const mapRef = useRef(null);
