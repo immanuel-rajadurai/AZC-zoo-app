83d7bcd07c684f5dc1d13d635a9ac7b67031c433
Benjamin Morka <benjamin@Benjamins-MacBook-Pro.local>
2024-08-23 11:57:53 +0100

    extract location info from app

--- app/index.jsx
+++ app/index.jsx
@@ -1,7 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
+import React, { useState, useEffect} from 'react'
 import { Text, View, Image, ScrollView } from 'react-native';
 import { Redirect, router, Router } from 'expo-router';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import * as Location from 'expo-location';
 
 import { images } from '../constants/';
 import CustomButton from '../components/CustomButton';
@@ -16,10 +18,53 @@ import { useGlobalContext } from '../context/GlobalProvider';
 //com.jsm.app_v1
 export default function App() {
 
+
   //  const { isLoading, isLoggedIn } = useGlobalContext();
 
   //  if (!isLoading && isLoggedIn) return <Redirect href="/home"/>
+  const [location, setLocation] = useState({ latitude: null, longitude: null });
+  const [lastLogTime, setLastLogTime] = useState(Date.now());
 
+  useEffect(() => {
+    let locationSubscription;
+
+    async function setupLocationTracking() {
+      const { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        console.log('Permission to access location was denied');
+        return;
+      }
+
+      const currentLocation = await Location.getCurrentPositionAsync({});
+      logLocation(currentLocation.coords);
+
+      locationSubscription = await Location.watchPositionAsync(
+        {
+          accuracy: Location.Accuracy.High,
+          timeInterval: 10000, // 10 seconds
+          distanceInterval: 0,
+        },
+        (newLocation) => logLocation(newLocation.coords)
+      );
+    }
+
+    function logLocation(coords) {
+      const currentTime = Date.now();
+      if (currentTime - lastLogTime >= 10000) {
+        console.log(`Latitude: ${coords.latitude}, Longitude: ${coords.longitude}`);
+        setLocation({ latitude: coords.latitude, longitude: coords.longitude });
+        setLastLogTime(currentTime);
+      }
+    }
+
+    setupLocationTracking();
+
+    return () => {
+      if (locationSubscription) {
+        locationSubscription.remove();
+      }
+    };
+  }, [lastLogTime]);
     return (
         <SafeAreaView className="bg-white h-full">
           <ScrollView contentContainerStyle={{ height: '100%'}}>
