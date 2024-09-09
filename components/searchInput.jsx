f10b2b570062d7346d76f36ede9069bfa943dcef
Muhammad-Chikhoun <mchikhoun@outlook.com>
2024-09-09 15:57:14 +0100

    Added functionality to the search feature
    After hitting enter, the search results are displayed in a scrollable modal. Currently uses the dummy data. For testing search "A" to get all events.

--- components/searchInput.jsx
+++ components/searchInput.jsx
@@ -1,11 +1,21 @@
-import { View, Text, TextInput, TouchableOpacity, Image } from 'react-native'
-import React, { useState,  forwardRef  } from 'react'
+import { View, Text, TextInput, TouchableOpacity, Image, FlatList, Modal, SafeAreaView, ScrollView, StyleSheet } from 'react-native';
+import React, { useState, forwardRef } from 'react';
+import eventsDummy from '../data/events.js';
+import { icons } from '../constants';
 
-import { icons } from '../constants'
+const SearchInput = forwardRef(({ title, value, placeholder, handleChangeText, otherStyles, ...props }, ref) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [results, setResults] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
-const SearchInput = forwardRef( ({title, value, placeholder, handleChangeText, otherStyles, ...props}, ref) => {
-
-  const [showPassword, setshowPassword] = useState(false)
+  // Function to handle search submission
+  const handleSearchSubmit = () => {
+    const filteredResults = eventsDummy.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+    setResults(filteredResults);
+    setModalVisible(true);
+  };
 
   return (
 
@@ -29,10 +39,112 @@ const SearchInput = forwardRef( ({title, value, placeholder, handleChangeText, o
             placeholder="Search for something"
             placeholderTextColor="#7b7b8b"
             onChangeText={handleChangeText}
+            onSubmitEditing={handleSearchSubmit}
             secureTextEntry={title === 'Passsword' && !showPassword}
         />
+        <Modal
+        visible={modalVisible}
+        transparent={true}
+        animationType="slide"
+        onRequestClose={() => setModalVisible(false)}
+        >
+        <SafeAreaView style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text style={styles.modalTitle}>Search Results</Text>
+            <ScrollView>
+            {results.length > 0 ? (
+              <FlatList
+                data={results}
+                keyExtractor={(item) => item.id}
+                renderItem={({ item }) => (
+                  <View style={styles.imagView}>
+                    <Image 
+                      source={{ uri: item.image }} 
+                      style={styles.animalImage} 
+                    />
+                    <View style={styles.textView}> 
+                      <Text style={styles.sectionTitle}>{item.name}</Text>
+                      <Text numberOfLines={3} style={styles.sectionText}>{item.description}</Text>
+                    </View>
+                  </View>
+                )}
+              />
+            ) : (
+              <Text>No results found</Text>
+            )}
+            </ScrollView>
+            <TouchableOpacity  onPress={() => setModalVisible(false)} style={styles.closeButton}>
+              <Text style={styles.closeButtonText}>Close</Text>
+            </TouchableOpacity>
+          </View>
+        </SafeAreaView>
+      </Modal>
       </View>
   )
 })
 
+const styles = StyleSheet.create({
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: '95%',
+    height: '75%',
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 10,
+    elevation: 5,
+  },
+  imagView:{
+    flexDirection: 'row', 
+    alignItems: 'center', 
+    marginVertical: 10
+  },
+  textView:{
+    flexDirection: 'column', 
+    alignItems: 'left', 
+    marginVertical: 10
+  },
+  modalTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: 'green',
+    marginBottom: 10,
+    marginTop: 5,
+    textAlign: "center"
+  },
+  animalImage: {
+    width: 80,
+    height: 80,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  sectionText: {
+    fontSize: 12,
+    color: 'black',
+    textAlign: "left",
+    maxWidth: 270
+  },
+  closeButton: {
+    backgroundColor: 'green',
+    padding: 10,
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  closeButtonText: {
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+});
 export default SearchInput
