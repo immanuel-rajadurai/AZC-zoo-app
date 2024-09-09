949eb5af5591dca933c7e8f150f0ceb5fdd31c33
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2024-09-09 17:38:14 +0100

    push

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -1,7 +1,33 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useState, useRef, useEffect } from 'react';
+import * as mobilenet from '@tensorflow-models/mobilenet';
+
 
 const Profile = () => {
+
+  useEffect(() => {
+
+    const loadModel = async () => {
+      // const mobilenet = require('@tensorflow-models/mobilenet');
+  
+      console.log(mobilenet);
+
+      console.log("loading model...");
+  
+      const model = await mobilenet.load();
+
+      console.log("model loaded: " + model);
+    };
+  
+
+    loadModel();
+  }, []);
+
+
+  
+  
+  
   return (
     <View>
       <Text>Profile page</Text>
