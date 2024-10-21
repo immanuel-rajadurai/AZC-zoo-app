08ec48f8bfc597428593c75e960b4339fb40db3b
Immanuel Rajadurai <irr24@ic.ac.uk>
2024-10-21 12:22:18 +0100

    rewrite the UI for the pull up tab button to conform to the wireframe depiction

---
+++ components/ToggleShowInformationButton.jsx
@@ -0,0 +1,60 @@
+import { View, Text, TouchableOpacity, StyleSheet  } from 'react-native'
+import React from 'react'
+
+const ToggleShowInformationButton = ({ title, handlePress, containerStyles, textStyles, isLoading}) => {
+  return (
+    <TouchableOpacity 
+      onPress={handlePress}
+      activeOpacity={0.7}
+      // className={`bg-green-500 rounded-xl min-h-[62px] justify-center items-center 
+      // ${containerStyles} ${isLoading ? 'opacity-50':''}`}
+      disabled={isLoading}
+      style={[
+        styles.button,
+        containerStyles,
+        isLoading ? styles.loading : null
+      ]}
+    >
+      <Text className={`text-primary font-pbold text-lg ${textStyles}`}>
+        ^
+      </Text>
+      <Text className={`text-primary font-pbold text-lg ${textStyles}`}>
+        {title}
+      </Text>
+      <Text className={`text-primary font-pbold text-sm ${textStyles}`}>
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 0,
+    width: '100%',
+    alignItems: 'center', // Center horizontally
+    zIndex: 1000,
+  },
+  button: {
+    backgroundColor: '#8BC34A',
+    borderTopLeftRadius: 100,
+    borderTopRightRadius: 100,
+    borderBottomLeftRadius: 30, // Higher border-radius for arch effect
+    borderBottomRightRadius: 30, // Higher border-radius for arch effect
+    height: 100,
+    width: '80%', // Adjust width as needed
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loading: {
+    opacity: 0.5,
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+
+export default ToggleShowInformationButton
