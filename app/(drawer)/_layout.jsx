072cda690871db351b0c41db0a2d0ef5351215d6
Immanuel Rajadurai <irr24@ic.ac.uk>
2024-10-22 11:04:19 +0100

    finalise drawer design and link first working browser page

--- app/(drawer)/_layout.jsx
+++ app/(drawer)/_layout.jsx
@@ -20,19 +20,105 @@ const CustomDrawerContent = (props) => {
             <Image source={icons.zoodrawericon} style={styles.icon} />
             <Text style={styles.topicText}>Your Visit</Text>
           </View>
-          <DrawerItem
-          label={"Wallet"}
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Tickets
+            </Text>
+          )}
           onPress={() => {
-              // router.push('wallet');
+              navigation.navigate('tickets');
+          }}
+          options={{ headerShown: false }}
+        />
+
+        <DrawerItem
+          label={"Membership"}
+          onPress={() => {
               navigation.navigate('wallet');
           }}
           options={{ headerShown: false }}
         />
 
         <DrawerItem
-          label={"Wallet"}
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Experiences
+            </Text>
+          )}
           onPress={() => {
-              // router.push('wallet');
+              navigation.navigate('wallet');
+          }}
+          options={{ headerShown: false }}
+        />
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Venue hire
+            </Text>
+          )}
+          onPress={() => {
+              navigation.navigate('wallet');
+          }}
+          options={{ headerShown: false }}
+        />
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Wallet (Add and see your tickets)
+            </Text>
+          )}
+          onPress={() => {
+              navigation.navigate('wallet');
+          }}
+          options={{ headerShown: false }}
+        />
+
+        <View style={styles.header}>
+          <Image source={icons.supporticon} style={styles.icon} />
+          <Text style={styles.topicText}>Support</Text>
+        </View>
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              FAQs
+            </Text>
+          )}
+          onPress={() => {
+              navigation.navigate('wallet');
+          }}
+          options={{ headerShown: false }}
+        />
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Contact us
+            </Text>
+          )}
+          onPress={() => {
+              navigation.navigate('wallet');
+          }}
+          options={{ headerShown: false }}
+        />
+
+
+        <View style={styles.header}>
+          <Image source={icons.privacyicon} style={styles.icon} />
+          <Text style={styles.topicText}>Privacy & Legal</Text>
+        </View>
+
+        <DrawerItem
+          label={() => (
+            <Text style={styles.drawerLabel}>
+              Policies
+            </Text>
+          )}
+          onPress={() => {
               navigation.navigate('wallet');
           }}
           options={{ headerShown: false }}
@@ -57,14 +143,19 @@ const styles = StyleSheet.create({
     margin: 10,
   },
   icon: {
-    width: 34,
-    height: 34,
+    width: 40,
+    height: 40,
     marginRight: 10,
   },
   topicText: {
     color: "green",
     fontWeight: "bold",
     fontSize: 20,
+  },
+  drawerLabel: {
+    flexWrap: 'wrap', 
+    color: 'grey',
+    fontWeight: "bold",
   },
 });
 
---
+++ app/tickets.jsx
@@ -0,0 +1,48 @@
+import { View, Text, StyleSheet, Button } from 'react-native';
+import React from 'react';
+import * as DocumentPicker from 'expo-document-picker';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import * as FileSystem from 'expo-file-system';
+import WebView from 'react-native-webview';
+
+const Tickets = () => {
+
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+        <WebView 
+            source={{ uri: 'https://zsllondonzoo.seetickets.com/zsl/zsl-london-zoo?_gl=1*1ad2eww*_gcl_au*MTcxMjM1MjUyNC4xNzI5NTg4NDE2*_ga*MTM4MjAzNTM0Ny4xNzI5NTg4NDE2*_ga_MWZNHV9X89*MTcyOTU4ODQxNS4xLjAuMTcyOTU4ODQxNS42MC4wLjA.'}}
+            style={{ flex: 1 }} 
+        />
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    backgroundColor: '#f5f5f5', 
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    color: '#333', 
+  },
+  button: {
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#007bff', 
+    borderRadius: 5,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
+
+export default Tickets;
