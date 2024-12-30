1991d20becb2a10643a8e3bfbc777b77f5f4414c
Saad Abdeen <k23093538@kcl.ac.uk>
2024-12-30 12:41:00 +0300

    link front to back

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -8,29 +8,26 @@ import CustomButtonBlack from '../../components/CustomButtonBlack';
 import { router } from 'expo-router';
 import CustomButton from '../../components/CustomButton'
 
-import { ListUsers, CreateUser } from "../../src/graphql/queries";
+import { listUsers, CreateUser } from "../../src/graphql/queries";
 
 import { generateClient } from 'aws-amplify/api';
-import { listEvents } from '../../src/graphql/queries';
 import { API, graphqlOperation } from 'aws-amplify';
 
 
 const client = generateClient();
 
 
-const listUsers2 = /* GraphQL */ `
-  query List {
-    listUsers {
-        items {
-        firstName
-        lastName
-        email
-        }
-    }
-    }
-`;
-
-
+// const listUsers2 = /* GraphQL */ `
+//   query List {
+//     listUsers {
+//         items {
+//         firstName
+//         lastName
+//         email
+//         }
+//     }
+//     }
+// `;
 
 const createUserTemplate = `
   mutation CreateUser($email: String!, $firstName: String!, $lastName: String!, $optedIn: Boolean!) {
@@ -42,20 +39,21 @@ const createUserTemplate = `
   }
 `;
 
-
 const SignUp = () => {
     const [agreeTerms, setAgreeTerms] = useState(false);
     const [stayUpdated, setStayUpdated] = useState(false);
     const [users, setUsers] = useState([]);
+    const [firstName, setFirstName] = useState('');
+    const [lastName, setLastName] = useState('');
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
 
     useEffect(() => {
         const fetchUsers = async () => {
             try {
                 const usersResult = await client.graphql(
-                    { query: listUsers2 }
+                    { query: listUsers }
                 );
-
-                // console.log(usersResult);
                 // console.log(usersResult.data.listUsers.items);
 
                 setUsers(usersResult.data.listUsers.items);
@@ -67,86 +65,50 @@ const SignUp = () => {
         fetchUsers();
     }, []);
 
+    const handleContinue = async () => {
+        try {
+            const inputVariables = {
+                firstName,
+                lastName,
+                email,
+                optedIn: stayUpdated,
+            };
 
+            const usersResult = await client.graphql({
+                query: createUserTemplate,
+                variables: inputVariables,
+            });
 
-    useEffect(() => {
-        const addTestUser = async () => {
-            console.log('Adding test user...');
-            try {
-             
-                let inputVariables = {
-                    firstName: "Jane4",
-                    lastName: "Doe",
-                    email: "janedoe4@gmail.com",
-                    optedIn: true
-                }
-            
-                const usersResult = await client.graphql({
-                    query: createUserTemplate,
-                    variables: inputVariables, 
-                });
-    
-                console.log('User created:', usersResult.data.createUser);
-            } catch (error) {
-                console.log('Error creating user:', error);
-            }
-        };
-
-        addTestUser();
-    }, []);
-
-    
-
-
-    //   const [form, setForm] = useState({
-//     firstName: '',
-//     lastName: '',
-//     username: '',
-//     email: '',
-//     // password: '',
-//   })
-
-//   const [isSubmitting, setIsSubmitting] = useState(false)
-
-//   const submit = async () => {
-//     if (!form.username || !form.email || !form.password) {
-//       Alert.alert("Error", "Please fill in all the fields")
-//     }
-
-    // setIsSubmitting(true)
-
-//     try {
-//       const result = await createUser(form.email, form.password, form.username)
-
-//       //set it to global state...
-
-//       router.replace('/home')
-//     } catch (error) {
-//       Alert.alert('Error', error.message)
-//     } finally {
-//       setIsSubmitting(false)
-//     }
-//   }
+            console.log('User created:', usersResult.data.createUser);
+            router.push('/home');
+        } catch (error) {
+            console.log('Error creating user:', error);
+        }
+    };
 
     return (
         <SafeAreaView className="h-full" backgroundColor='#234e35'>
-            <ScrollView contentContainerStyle={{ height: '100%'}}>
+            <ScrollView contentContainerStyle={{ height: '100%' }}>
                 <View className="w-full justify-center items-center min-h-[85vh] px-4" backgroundColor='#234e35'>
                     <Text className="text-2xl text-white text-semibold mt-10 font-psemibold text-center">
                         Be the first to hear about special offers and events!
                     </Text>
 
                     <View className="w-full flex-row mt-4">
-                        <FormField title="First Name" otherStyles="flex-1 mr-4" />
-                        <FormField title="Last Name" otherStyles="flex-1" />
+                        <FormField title="First Name" value={firstName} handleChangeText={setFirstName} otherStyles="flex-1 mr-4" />
+                        <FormField title="Last Name" value={lastName} handleChangeText={setLastName} otherStyles="flex-1" />
                     </View>
 
                     <FormField
                         title="Username"
+                        value={username}
+                        handleChangeText={setUsername}
                         otherStyles="mt-4"
                     />
                     <FormField
                         title="Email"
+                        value={email}
+                        handleChangeText={setEmail}
                         keyboardType="email-address"
                         otherStyles="mt-4"
                     />
@@ -155,16 +117,17 @@ const SignUp = () => {
                         <View className="flex-row items-center mb-2">
                             <Checkbox value={agreeTerms} onValueChange={setAgreeTerms} color={'#16a34a'} />
                             <Text className="ml-2 text-white"> I agree with the{' '}
-                        <Text
-                            style={{ color: 'lightgray', textDecorationLine: 'underline'  }}
-                            onPress={() => Linking.openURL('https://google.com')}
-                        >
-                            Terms and Conditions
-                        </Text></Text>
+                                <Text
+                                    style={{ color: 'lightgray', textDecorationLine: 'underline' }}
+                                    onPress={() => Linking.openURL('https://google.com')}
+                                >
+                                    Terms and Conditions
+                                </Text>
+                            </Text>
                         </View>
 
                         <View className="flex-row items-center">
-                            <Checkbox value={stayUpdated} onValueChange={setStayUpdated} color={ '#16a34a'} />
+                            <Checkbox value={stayUpdated} onValueChange={setStayUpdated} color={'#16a34a'} />
                             <Text className="ml-2 text-white">
                                 Stay up to date with the latest news, deals and upcoming events
                             </Text>
@@ -173,7 +136,7 @@ const SignUp = () => {
 
                     <CustomButtonBlack
                         title="Continue"
-                        handlePress={() => router.push('/home')}
+                        handlePress={handleContinue}
                         containerStyles="w-full mt-9"
                     />
 
