4057f1cf36ec6deb0da13b41f52e268a25d98ce9
Immanuel Rajadurai <immanuel.rajadurai@kcl.ac.uk>
2025-03-30 10:33:17 +0100

    update app.config.js

--- app.config.js
+++ app.config.js
@@ -18,6 +18,7 @@ export default {
         supportsTablet: true
       },
       android: {
+        package: "com.immraj.app_v1", 
         adaptiveIcon: {
           foregroundImage: "./assets/adaptive-icon.png",
           backgroundColor: "#FFFFFF"
